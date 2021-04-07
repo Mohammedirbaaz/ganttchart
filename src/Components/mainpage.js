cca9c0850806125fe8f014a8ac76a57235f60b41
Mohammedirbaaz <58600867+Mohammedirbaaz@users.noreply.github.com>
2021-04-07 23:26:38 +0530

    display in accurate place

--- src/Components/mainpage.js
+++ src/Components/mainpage.js
@@ -5,6 +5,7 @@ let starts=0;
 let ends=0;
 let remains=0;
 let total=0;
+let finds=0;
 
 
 class mainpage extends Component{
@@ -41,6 +42,7 @@ class mainpage extends Component{
             starts=finaldate;
             return 6;
         }
+
     }
     checkenddate(enddate){
         let dates = enddate;
@@ -51,8 +53,10 @@ class mainpage extends Component{
         parseInt(finaldate2);
         ends=finaldate2;
         let remaining=ends-starts+1;
-        console.log("remaining "+remaining);
+        // console.log("remaining "+remaining);
         remains=remaining;
+        
+
     }
     changebg=(idss,start,rem)=>{
         
@@ -64,15 +68,36 @@ class mainpage extends Component{
             ids.style.width="10%";
             ids.style.height="10px";
             ids.style.marginTop="0.5%";
+            
+            if(start>1 && start<5){
+                
+                // if(start!=1 && start!=5){
+                 finds=start;
+                // }
+                //|| start>6 && start<10 || start>11 && start<15 || start>16 && start<20 || start>21 && start<25
+                //|| start==10 || start==15 || start==20 || start==25 || start==30
+            }
+            else if(start>=6 && start<=10){
+                //  alert("from 6 to 10 "+start);
+                 finds=start;
+            }
+            else if(start==="05" || start==="10"){
+                //  alert("from  5 "+start);
+                 finds=start-1;
+                
+            }else if(start==6 || start==11 || start==16 || start==21 || start==26){
+                 finds=0;
+            }
+                
+            finds=finds*20;
+            // console.log("check me "+finds+" "+start);
+            ids.style.marginLeft=finds+"%";
 
             let finder1=rem/4.9;
             finder1=finder1*100;
-            // alert(finder1);
             ids.style.width=finder1+"%";
         },1000)
     }
-
-
 
     render(){ 
         return(
@@ -92,12 +117,13 @@ class mainpage extends Component{
                 {this.state.tasks.map((temp,index)=>
                     <tr key={index}>
                         <td>{temp.Name}</td>
-                        <td ><div id={"throwsid"+index}> {this.checkstartdate(temp.StartTime)===1 && <>{this.checkenddate(temp.EndTime)} {this.changebg("throwsid"+index,starts,remains)}</>}</div></td>
+                        <td ><div id={"throwsid1"+index}> {this.checkstartdate(temp.StartTime)===1 && <>{this.checkenddate(temp.EndTime)} {this.changebg("throwsid1"+index,starts,remains)}</>} </div></td>
                         <td ><div id={"throwsid2"+index}> {this.checkstartdate(temp.StartTime)===2 && <>{this.checkenddate(temp.EndTime)} {this.changebg("throwsid2"+index,starts,remains)}</>}</div></td>
                         <td ><div id={"throwsid3"+index}> {this.checkstartdate(temp.StartTime)===3 && <>{this.checkenddate(temp.EndTime)} {this.changebg("throwsid3"+index,starts,remains)}</>}</div></td>
                         <td > <div id={"throwsid4"+index}> {this.checkstartdate(temp.StartTime)===4 && <>{this.checkenddate(temp.EndTime)} {this.changebg("throwsid4"+index,starts,remains)}</>}</div></td>
                         <td >< div id={"throwsid5"+index}> {this.checkstartdate(temp.StartTime)===5 && <>{this.checkenddate(temp.EndTime)} {this.changebg("throwsid5"+index,starts,remains)}</>}</div></td>
                         <td ><div id={"throwsid6"+index}> {this.checkstartdate(temp.StartTime)===6 && <>{this.checkenddate(temp.EndTime)} {this.changebg("throwsid6"+index,starts,remains)}</>}</div></td>
+                    
                     </tr>
                 )}
 
