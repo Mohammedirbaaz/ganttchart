7aeec775ec24a13239adc008e3c0b71ddec9c4b5
Mohammedirbaaz <58600867+Mohammedirbaaz@users.noreply.github.com>
2021-04-07 19:58:01 +0530

    shows accurate

--- src/Components/mainpage.js
+++ src/Components/mainpage.js
@@ -21,7 +21,7 @@ class mainpage extends Component{
         let datesnew2=dates.charAt(1);
         let finaldate=datesnew1+datesnew2;
         parseInt(finaldate);
-        console.log(finaldate);
+        // console.log(finaldate);
         if(finaldate>=1 && finaldate<=5){
             starts=finaldate;
             return 1;
@@ -49,32 +49,26 @@ class mainpage extends Component{
         let datesnew2=dates.charAt(1);
         let finaldate2=datesnew1+datesnew2;
         parseInt(finaldate2);
-        console.log(finaldate2);
         ends=finaldate2;
         let remaining=ends-starts+1;
         console.log("remaining "+remaining);
         remains=remaining;
-        
-
     }
     changebg=(idss,start,rem)=>{
         
         setTimeout(()=>{
             let ids=document.getElementById(idss);
        
-            ids.style.position="absolute";
+            ids.style.position="relative";
             ids.style.backgroundColor="blue";
             ids.style.width="10%";
-            ids.style.height="10%";
+            ids.style.height="10px";
             ids.style.marginTop="0.5%";
 
-
-           
-            let finder1=rem/3.6;
-            finder1=finder1*10;
-
+            let finder1=rem/4.9;
+            finder1=finder1*100;
+            // alert(finder1);
             ids.style.width=finder1+"%";
-
         },1000)
     }
 
@@ -98,12 +92,12 @@ class mainpage extends Component{
                 {this.state.tasks.map((temp,index)=>
                     <tr key={index}>
                         <td>{temp.Name}</td>
-                        <td id={"throwsid"+index}> {this.checkstartdate(temp.StartTime)===1 && <>{this.checkenddate(temp.EndTime)} {this.changebg("throwsid"+index,starts,remains)}</>}</td>
-                        <td id={"throwsid2"+index}> {this.checkstartdate(temp.StartTime)===2 && <>{this.checkenddate(temp.EndTime)} {this.changebg("throwsid2"+index,starts,remains)}</>}</td>
-                        <td id={"throwsid3"+index}> {this.checkstartdate(temp.StartTime)===3 && <>{this.checkenddate(temp.EndTime)} {this.changebg("throwsid3"+index,starts,remains)}</>}</td>
-                        <td id={"throwsid4"+index}> {this.checkstartdate(temp.StartTime)===4 && <>{this.checkenddate(temp.EndTime)} {this.changebg("throwsid4"+index,starts,remains)}</>}</td>
-                        <td id={"throwsid5"+index}> {this.checkstartdate(temp.StartTime)===5 && <>{this.checkenddate(temp.EndTime)} {this.changebg("throwsid5"+index,starts,remains)}</>}</td>
-                        <td id={"throwsid6"+index}> {this.checkstartdate(temp.StartTime)===6 && <>{this.checkenddate(temp.EndTime)} {this.changebg("throwsid6"+index,starts,remains)}</>}</td>
+                        <td ><div id={"throwsid"+index}> {this.checkstartdate(temp.StartTime)===1 && <>{this.checkenddate(temp.EndTime)} {this.changebg("throwsid"+index,starts,remains)}</>}</div></td>
+                        <td ><div id={"throwsid2"+index}> {this.checkstartdate(temp.StartTime)===2 && <>{this.checkenddate(temp.EndTime)} {this.changebg("throwsid2"+index,starts,remains)}</>}</div></td>
+                        <td ><div id={"throwsid3"+index}> {this.checkstartdate(temp.StartTime)===3 && <>{this.checkenddate(temp.EndTime)} {this.changebg("throwsid3"+index,starts,remains)}</>}</div></td>
+                        <td > <div id={"throwsid4"+index}> {this.checkstartdate(temp.StartTime)===4 && <>{this.checkenddate(temp.EndTime)} {this.changebg("throwsid4"+index,starts,remains)}</>}</div></td>
+                        <td >< div id={"throwsid5"+index}> {this.checkstartdate(temp.StartTime)===5 && <>{this.checkenddate(temp.EndTime)} {this.changebg("throwsid5"+index,starts,remains)}</>}</div></td>
+                        <td ><div id={"throwsid6"+index}> {this.checkstartdate(temp.StartTime)===6 && <>{this.checkenddate(temp.EndTime)} {this.changebg("throwsid6"+index,starts,remains)}</>}</div></td>
                     </tr>
                 )}
 
