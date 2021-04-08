ca7f5cf71ff919ffab27f931706e486e920ab38e
Mohammedirbaaz <58600867+Mohammedirbaaz@users.noreply.github.com>
2021-04-08 11:35:11 +0530

    started working on updating data

--- src/Components/mainpage.js
+++ src/Components/mainpage.js
@@ -22,11 +22,12 @@ class mainpage extends Component{
         let datesnew2=dates.charAt(1);
         let finaldate=datesnew1+datesnew2;
         parseInt(finaldate);
+
         // console.log(finaldate);
         if(finaldate>=1 && finaldate<=5){
             starts=finaldate;
             return 1;
-        }else if(finaldate<=6 && finaldate<=10){
+        }else if(finaldate>=6 && finaldate<=10){
             starts=finaldate;
             return 2;
         }else if(finaldate>=11 && finaldate<=15){
@@ -53,50 +54,66 @@ class mainpage extends Component{
         parseInt(finaldate2);
         ends=finaldate2;
         let remaining=ends-starts+1;
-        // console.log("remaining "+remaining);
         remains=remaining;
         
 
     }
-    changebg=(idss,start,rem)=>{
+
+    changebg=(idss,start,rem,ind)=>{
         
         setTimeout(()=>{
             let ids=document.getElementById(idss);
+            ids.addEventListener("mouseover",()=>{
+                let statuss=document.getElementById("statusid"+ind);
+                statuss.style.visibility="visible";
+            })
+            ids.addEventListener("mouseout",()=>{
+                let statuss=document.getElementById("statusid"+ind);
+                statuss.style.visibility="hidden";
+            })
        
             ids.style.position="relative";
             ids.style.backgroundColor="blue";
             ids.style.width="10%";
             ids.style.height="10px";
             ids.style.marginTop="0.5%";
-            
-            if(start>1 && start<5){
-                
-                // if(start!=1 && start!=5){
-                 finds=start;
-                // }
-                //|| start>6 && start<10 || start>11 && start<15 || start>16 && start<20 || start>21 && start<25
-                //|| start==10 || start==15 || start==20 || start==25 || start==30
+            let frists=0;
+            if((start>1 && start<5)){
+                frists=1;
             }
-            else if(start>=6 && start<=10){
-                //  alert("from 6 to 10 "+start);
-                 finds=start;
+            else if(start>6 && start<10){
+                 frists=6; 
             }
-            else if(start==="05" || start==="10"){
-                //  alert("from  5 "+start);
-                 finds=start-1;
-                
-            }else if(start==6 || start==11 || start==16 || start==21 || start==26){
-                 finds=0;
+            else if(start>11 && start<15){
+                frists=11; 
+           }
+            else if(start>16 && start<20){
+                frists=16;  
             }
+            else if(start>21 && start<25){
+                frists=21;  
+            }
+            else if(start==="05" || start==="10" || start==="15" || start==="20" || start==="25" || start==="30"){
+                finds=start-1;
+            }
+            else if(start==1 || start==6 || start==11 || start==16 || start==21 || start==26){
+                finds=0;
+            }
+            frists=start-frists;
+            finds=frists;
                 
             finds=finds*20;
-            // console.log("check me "+finds+" "+start);
             ids.style.marginLeft=finds+"%";
 
             let finder1=rem/4.9;
             finder1=finder1*100;
             ids.style.width=finder1+"%";
         },1000)
+    }
+    changedetails=(idcss)=>{
+        let findview=document.getElementById("cardviewid");
+        findview.style.visibility="visible";
+
     }
 
     render(){ 
@@ -117,18 +134,35 @@ class mainpage extends Component{
                 {this.state.tasks.map((temp,index)=>
                     <tr key={index}>
                         <td>{temp.Name}</td>
-                        <td ><div id={"throwsid1"+index}> {this.checkstartdate(temp.StartTime)===1 && <>{this.checkenddate(temp.EndTime)} {this.changebg("throwsid1"+index,starts,remains)}</>} </div></td>
-                        <td ><div id={"throwsid2"+index}> {this.checkstartdate(temp.StartTime)===2 && <>{this.checkenddate(temp.EndTime)} {this.changebg("throwsid2"+index,starts,remains)}</>}</div></td>
-                        <td ><div id={"throwsid3"+index}> {this.checkstartdate(temp.StartTime)===3 && <>{this.checkenddate(temp.EndTime)} {this.changebg("throwsid3"+index,starts,remains)}</>}</div></td>
-                        <td > <div id={"throwsid4"+index}> {this.checkstartdate(temp.StartTime)===4 && <>{this.checkenddate(temp.EndTime)} {this.changebg("throwsid4"+index,starts,remains)}</>}</div></td>
-                        <td >< div id={"throwsid5"+index}> {this.checkstartdate(temp.StartTime)===5 && <>{this.checkenddate(temp.EndTime)} {this.changebg("throwsid5"+index,starts,remains)}</>}</div></td>
-                        <td ><div id={"throwsid6"+index}> {this.checkstartdate(temp.StartTime)===6 && <>{this.checkenddate(temp.EndTime)} {this.changebg("throwsid6"+index,starts,remains)}</>}</div></td>
+                        <td ><div  id={"throwsid1"+index} > {this.checkstartdate(temp.StartTime)===1 && <>{this.checkenddate(temp.EndTime)} {this.changebg("throwsid1"+index,starts,remains,index)} <span className="status" id={"statusid"+index}>{temp.Status}<button onClick={()=>{this.changedetails("throwsid1"+index)}}>Change</button></span> </>} </div></td>
+                        <td ><div id={"throwsid2"+index}> {this.checkstartdate(temp.StartTime)===2 && <>{this.checkenddate(temp.EndTime)} {this.changebg("throwsid2"+index,starts,remains,index)} <span className="status" id={"statusid"+index}>{temp.Status}<button onClick={()=>{this.changedetails("throwsid2"+index)}}>Change</button></span></>}</div></td>
+                        <td ><div id={"throwsid3"+index}> {this.checkstartdate(temp.StartTime)===3 && <>{this.checkenddate(temp.EndTime)} {this.changebg("throwsid3"+index,starts,remains,index)} <span className="status" id={"statusid"+index}>{temp.Status}<button onClick={()=>{this.changedetails("throwsid3"+index)}}>Change</button></span></>}</div></td>
+                        <td > <div id={"throwsid4"+index}> {this.checkstartdate(temp.StartTime)===4 && <>{this.checkenddate(temp.EndTime)} {this.changebg("throwsid4"+index,starts,remains,index)} <span className="status" id={"statusid"+index}>{temp.Status}<button onClick={()=>{this.changedetails("throwsid4"+index)}}>Change</button></span></>}</div></td>
+                        <td >< div id={"throwsid5"+index}> {this.checkstartdate(temp.StartTime)===5 && <>{this.checkenddate(temp.EndTime)} {this.changebg("throwsid5"+index,starts,remains,index)} <span className="status" id={"statusid"+index}>{temp.Status}<button onClick={()=>{this.changedetails("throwsid5"+index)}}>Change</button></span></>}</div></td>
+                        <td ><div id={"throwsid6"+index}> {this.checkstartdate(temp.StartTime)===6 && <>{this.checkenddate(temp.EndTime)} {this.changebg("throwsid6"+index,starts,remains,index)} <span className="status" id={"statusid"+index}>{temp.Status}<button onClick={()=>{this.changedetails("throwsid6"+index)}}>Change</button></span></>}</div></td>
                     
                     </tr>
                 )}
 
             
             </table>
+
+            <div className="cardview" id="cardviewid">
+                    <p className="headers">Change Details</p>
+                    <table>
+                        <tr>
+                            <th>Start</th>
+                            <td id="startid"><input type="date"/></td>
+                        </tr>
+                        <tr>
+                            <th>End</th>
+                            
+                        </tr>
+                        <tr>
+                        <th>Status</th>
+                        </tr>
+                    </table>
+            </div>
         </div>
           );
     }
